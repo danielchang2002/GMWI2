627996a7dd25dd0463e6f3283523ec51117c4c93
danielchang2002 <danielchang2002@gmail.com>
2022-08-17 14:53:16 -0500

    :zap: tuning

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ RAW_DIR = "../data/raw"
 CLEAN_DIR = "../data/clean"
 MODEL_DIR = "../output/models"
 FIGURE_DIR = "../output/figures"
-PREDICTION_DIR = "../output/predictions"
+LOG_DIR = "../output/logs"
 
 # Model hyperparameters
 PRESENCE_CUTOFF = 0.00001
