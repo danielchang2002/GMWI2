0ecc76db0ba770ab5ba74c5ddca0b5b328528616
danielchang2002 <danielchang2002@gmail.com>
2022-09-04 13:29:11 -0500

    figures

--- src/utils.py
+++ src/utils.py
@@ -1,51 +1,83 @@
 import numpy as np
-from scipy.stats import wilcoxon
+from scipy.stats import wilcoxon, kendalltau
 import matplotlib.pyplot as plt
 import os
 import config
 
-def plot_longitudinal(meta, index, timepoints_sorted, study):
+def plot_longitudinal(meta, indices, timepoints_sorted, study, stars=False, show_tau=False):
     """
     meta is a df that has columns: ["timepoint", "timepoint_plot", index]
     """
-    subjects = np.unique(meta.index)
-    plt.figure(figsize=(10, 10), dpi=200)
-    for subject in subjects:
-        df = meta.loc[subject].copy()
-        df = df.sort_values("timepoint_plot")
-        plt.plot(df["timepoint_plot"], df[index], c="gray", zorder=0)
-    plt.ylabel(index, fontsize=18)
-    plt.xlabel("Time point", fontsize=18)
+    fig, axs = plt.subplots(ncols=len(indices), figsize=(10 * len(indices), 10), dpi=200)
+    for index, ax in zip(indices, axs):
+        subjects = np.unique(meta.index)
+        for subject in subjects:
+            df = meta.loc[[subject]].copy()
+            df = df.sort_values("timepoint_plot")
+            ax.plot(df["timepoint_plot"], df[index], c="gray", zorder=0)
+        ax.set_ylabel(index, fontsize=18)
+        ax.set_xlabel("Time point", fontsize=18)
 
-    for i, time in enumerate(timepoints_sorted):
-        data = meta[meta["timepoint"] == time][index]
-        sc = plt.scatter(np.ones(data.shape[0]) * i, data)
-        c = sc.get_facecolors()[0].tolist()
-        width = 3.0
-        bp = plt.boxplot(data, positions=[i], patch_artist=True, widths=[0.5],
-            boxprops=dict(facecolor=[0, 0, 0, 0], color=c, linewidth=width),
-            capprops=dict(color=c, linewidth=0),
-            whiskerprops=dict(color=c, linewidth=width),
-            flierprops=dict(color=c, markeredgecolor=c, linewidth=width),
-            medianprops=dict(color=c, linewidth=width),
-                        )
-    baseline = timepoints_sorted[0]
-    baseline_df = meta[meta["timepoint"] == baseline]
-    for i, timepoint in enumerate(timepoints_sorted):
-        if i == 0: continue
-        timepoint_df = meta[meta["timepoint"] == timepoint]
-        subjects_in_both = set(baseline_df.index) & set(timepoint_df.index)
-        timepoint_df = timepoint_df.loc[subjects_in_both]
-        baseline_df = baseline_df.loc[subjects_in_both]
-        stat = wilcoxon(baseline_df[index], timepoint_df[index])
-        x = i
-        y = meta[index].max() * 1.05
-        text = "$P = " + '%.2g' % stat.pvalue + "$" if stat.pvalue < 0.05 else "n.s."
-        plt.text(x, y, text, ha="center", fontsize=12)
+        for i, time in enumerate(timepoints_sorted):
+            data = meta[meta["timepoint"] == time][index]
+            sc = ax.scatter(np.ones(data.shape[0]) * i, data)
+            c = sc.get_facecolors()[0].tolist()
+            width = 3.0
+            bp = ax.boxplot(data, positions=[i], patch_artist=True, widths=[0.5],
+                boxprops=dict(facecolor=[0, 0, 0, 0], color=c, linewidth=width),
+                capprops=dict(color=c, linewidth=0),
+                whiskerprops=dict(color=c, linewidth=width),
+                flierprops=dict(color=c, markeredgecolor=c, linewidth=width),
+                medianprops=dict(color=c, linewidth=width),
+                            )
 
-    plt.xticks(ticks=list(range(len(timepoints_sorted))), labels=timepoints_sorted, fontsize=14)
-    plt.yticks(fontsize=14)
-    bottom, top = plt.ylim()
-    plt.ylim(bottom, top * 1.05)
-    plt.savefig(os.path.join(config.FIGURE_DIR, study + "_" + index.replace(" ", "_") + ".svg"))
-    plt.show()
+        ran = meta[index].max() - meta[index].min()
+
+        y = meta[index].max() + ran * 0.05
+        y2 = meta[index].max() + ran * 0.1
+
+        baseline = timepoints_sorted[0]
+        baseline_df = meta[meta["timepoint"] == baseline]
+        for i, timepoint in enumerate(timepoints_sorted):
+            if i == 0: continue
+            timepoint_df = meta[meta["timepoint"] == timepoint]
+            subjects_in_both = set(baseline_df.index) & set(timepoint_df.index)
+            timepoint_df = timepoint_df.loc[subjects_in_both]
+            baseline_df_curr = baseline_df.loc[subjects_in_both]
+            if timepoint_df.shape[0] == 0 or baseline_df_curr.shape[0] == 0:
+                wilcoxon_p = 1
+                tau_p = 1
+            else:
+                stat = wilcoxon(baseline_df_curr[index], timepoint_df[index])
+                wilcoxon_p = stat.pvalue
+                stat = kendalltau(baseline_df_curr[index], timepoint_df[index])
+                tau, tau_p = stat
+                
+            x = i
+            
+            if stars:
+                text = "****" if wilcoxon_p <= 0.0001 else "***" if wilcoxon_p <= 0.001 else "**" if wilcoxon_p <= 0.01 else "*" if wilcoxon_p <= 0.05 else "n.s."
+            else:
+                text = "$P = " + '%.2g' % wilcoxon_p + "$" if wilcoxon_p < 0.05 else "n.s."
+            ax.text(x, y, text, ha="center", fontsize=14)
+            
+            if show_tau and tau_p < 0.05:
+                ax.text(x, y2, r"$\tau$ = " + '%.2g' % tau, ha="center", fontsize=14)
+
+        ax.set_xticks(ticks=list(range(len(timepoints_sorted))))
+        ax.set_xticklabels(labels=timepoints_sorted, fontsize=14)
+        ax.tick_params(axis="y", labelsize=14)
+        bottom, top = ax.get_ylim()
+        ax.set_ylim(bottom, ran * 0.05 + (y2 if show_tau else y))
+
+    plt.savefig(os.path.join(config.FIGURE_DIR, study + ".svg"))
+
+
+def get_diversity(meta, X):
+    X_species = X[[col for col in X.columns if "s__" in col and "virus" not in col and "unclassified" not in col]]
+    meta = meta.copy()
+    meta["Species Richness"] = np.sum(X_species > config.PRESENCE_CUTOFF, axis=1).values
+    meta["Shannon Diversity"] = np.sum(-1 * (X_species * np.log(X_species)).fillna(0).values, axis=1)
+    meta["Simpson Diversity"] = np.sum((X_species ** 2).values, axis=1)
+    return meta
+
